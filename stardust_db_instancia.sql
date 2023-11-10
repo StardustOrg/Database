0da11fcbe3ff201185c4f504e6d6591d5eff2ecd
yanna-torres <torres.yanna03@gmail.com>
2023-11-10 08:47:12 -0300

    data updated

--- stardust_db_instancia.sql
+++ stardust_db_instancia.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 16.0
 -- Dumped by pg_dump version 16.0
 
--- Started on 2023-11-09 17:43:54
+-- Started on 2023-11-10 08:46:37
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -24,34 +24,32 @@ SET row_security = off;
 -- Data for Name: artist; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.artist (id, name, icon_url, cover_url, group_id) FROM stdin;
-1	NewJeans	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/newjeans_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/nj_cover.jpg	\N
-2	Minji	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/minji_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/minji_cover.jpg	1
-3	Hanni	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/hanni_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/hanni_cover.jpg	1
-4	Danielle	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/dani_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/dani_cover.jpg	1
-5	Haerin	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/haerin_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/haerin_cover.jpg	1
-6	Hyein	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/hyein_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/hyein_cover.jpg	1
-7	aespa	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/aespa_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/aespa_cover.jpg	\N
-8	Karina	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/karina_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/karina_cover.jpg	7
-9	Giselle	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/giselle_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/giselle_cover.jpg	7
-10	Winter	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/winter_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/winter_cover.jpg	7
-11	NingNing	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/ningning_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/ningning_cover.jpg	7
-12	IU	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/iu/iu_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/iu/iu_cover.jpg	\N
-13	SEVENTEEN	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/svt_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/svt_cover.jpg	\N
-14	S.coups	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/scoups_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/scoups_cover.jpg	13
-15	Jeonghan	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/jeonghan_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/jeonghan_cover.jpg	13
-16	Joshua	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/joshua_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/joshua_cover.jpg	13
-17	Jun	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/jun_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/jun_cover.jpg	13
-18	Hoshi	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/hoshi_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/hoshi_cover.jpg	13
-19	Wonwoo	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/wonwoo_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/wonwoo_cover.jpg	13
-20	Woozi	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/woozi_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/woozi_cover.jpg	13
-21	DK	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/dk_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/dk_cover.jpg	13
-22	Mingyu	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/mingyu_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/mingyu_cover.jpg	13
-23	The8	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/the8_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/the8_cover.jpg	13
-24	Seungkwan	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/seungkwan_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/seungkwan_cover.jpg	13
-25	Vernon	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/vernon_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/vernon_cover.jpg	13
-26	Dino	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/dino_icon.jpg	https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/dino_cover.jpg	13
-\.
+INSERT INTO public.artist VALUES (1, 'NewJeans', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/newjeans_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/nj_cover.jpg', NULL);
+INSERT INTO public.artist VALUES (2, 'Minji', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/minji_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/minji_cover.jpg', 1);
+INSERT INTO public.artist VALUES (3, 'Hanni', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/hanni_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/hanni_cover.jpg', 1);
+INSERT INTO public.artist VALUES (4, 'Danielle', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/dani_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/dani_cover.jpg', 1);
+INSERT INTO public.artist VALUES (5, 'Haerin', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/haerin_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/haerin_cover.jpg', 1);
+INSERT INTO public.artist VALUES (6, 'Hyein', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/hyein_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/nj/hyein_cover.jpg', 1);
+INSERT INTO public.artist VALUES (7, 'aespa', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/aespa_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/aespa_cover.jpg', NULL);
+INSERT INTO public.artist VALUES (8, 'Karina', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/karina_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/karina_cover.jpg', 7);
+INSERT INTO public.artist VALUES (9, 'Giselle', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/giselle_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/giselle_cover.jpg', 7);
+INSERT INTO public.artist VALUES (10, 'Winter', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/winter_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/winter_cover.jpg', 7);
+INSERT INTO public.artist VALUES (11, 'NingNing', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/ningning_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/aespa/ningning_cover.jpg', 7);
+INSERT INTO public.artist VALUES (12, 'IU', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/iu/iu_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/iu/iu_cover.jpg', NULL);
+INSERT INTO public.artist VALUES (13, 'SEVENTEEN', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/svt_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/svt_cover.jpg', NULL);
+INSERT INTO public.artist VALUES (14, 'S.coups', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/scoups_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/scoups_cover.jpg', 13);
+INSERT INTO public.artist VALUES (15, 'Jeonghan', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/jeonghan_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/jeonghan_cover.jpg', 13);
+INSERT INTO public.artist VALUES (16, 'Joshua', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/joshua_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/joshua_cover.jpg', 13);
+INSERT INTO public.artist VALUES (17, 'Jun', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/jun_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/jun_cover.jpg', 13);
+INSERT INTO public.artist VALUES (18, 'Hoshi', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/hoshi_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/hoshi_cover.jpg', 13);
+INSERT INTO public.artist VALUES (19, 'Wonwoo', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/wonwoo_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/wonwoo_cover.jpg', 13);
+INSERT INTO public.artist VALUES (20, 'Woozi', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/woozi_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/woozi_cover.jpg', 13);
+INSERT INTO public.artist VALUES (21, 'DK', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/dk_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/dk_cover.jpg', 13);
+INSERT INTO public.artist VALUES (22, 'Mingyu', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/mingyu_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/mingyu_cover.jpg', 13);
+INSERT INTO public.artist VALUES (23, 'The8', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/the8_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/the8_cover.jpg', 13);
+INSERT INTO public.artist VALUES (24, 'Seungkwan', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/seungkwan_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/seungkwan_cover.jpg', 13);
+INSERT INTO public.artist VALUES (25, 'Vernon', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/vernon_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/vernon_cover.jpg', 13);
+INSERT INTO public.artist VALUES (26, 'Dino', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/dino_icon.jpg', 'https://raw.githubusercontent.com/StardustOrg/stardust-assets/main/svt/dino_cover.jpg', 13);
 
 
 --
@@ -60,8 +58,6 @@ COPY public.artist (id, name, icon_url, cover_url, group_id) FROM stdin;
 -- Data for Name: product; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.product (id, description, amount, picture_path, price) FROM stdin;
-\.
 
 
 --
@@ -70,8 +66,6 @@ COPY public.product (id, description, amount, picture_path, price) FROM stdin;
 -- Data for Name: product_artist; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.product_artist (product_id, artist_id) FROM stdin;
-\.
 
 
 --
@@ -80,10 +74,8 @@ COPY public.product_artist (product_id, artist_id) FROM stdin;
 -- Data for Name: stardust_user; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.stardust_user (id, login, email, admin, password, address, name) FROM stdin;
-2	admin01	admin@admin.teste	t	123456	Rua A, 123	Leo Oliveira
-3	teste02	tes0ads@teste.com	f	123456	Rua 123	Yanna Teste
-\.
+INSERT INTO public.stardust_user VALUES (2, 'admin01', 'admin@admin.teste', true, '123456', 'Rua A, 123', 'Leo Oliveira');
+INSERT INTO public.stardust_user VALUES (3, 'teste02', 'tes0ads@teste.com', false, '123456', 'Rua 123', 'Yanna Teste');
 
 
 --
@@ -92,8 +84,6 @@ COPY public.stardust_user (id, login, email, admin, password, address, name) FRO
 -- Data for Name: sale; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.sale (id, user_id, date_time) FROM stdin;
-\.
 
 
 --
@@ -102,8 +92,6 @@ COPY public.sale (id, user_id, date_time) FROM stdin;
 -- Data for Name: sale_product; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.sale_product (sale_id, product_id, quantity) FROM stdin;
-\.
 
 
 --
@@ -142,7 +130,7 @@ SELECT pg_catalog.setval('public.sale_id_seq', 1, false);
 SELECT pg_catalog.setval('public.stardust_user_id_seq', 5, true);
 
 
--- Completed on 2023-11-09 17:43:54
+-- Completed on 2023-11-10 08:46:38
 
 --
 -- PostgreSQL database dump complete
